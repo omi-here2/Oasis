29e7641c836707a1e9406fb32a358d8202edf9c9
sarthakg043 <sarthakg4953@gmail.com>
2023-09-13 12:17:14 +0530

    Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
 import './App.css';
-import InputBox from './Components/inputbox/inputbox';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './Components/About';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import Navbar from './Components/Navbar';
+import ErrorPage from './ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "home",
+        element: <Home />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "signup",
+        element: <Signup />
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-     <InputBox />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact from './routes/contacts';
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
     <App />
   </React.StrictMode>,
 )
